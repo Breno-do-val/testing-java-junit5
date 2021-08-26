1e995db65ddae6f3373affc93c2abf19a0ab30b9
Breno Ribeiro do Val <brenorvale@gmail.com>
2021-08-26 09:44:55 -0300

    (test): add throwsException assertion test

--- src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.controllers.exceptions.OupsHandlerWithNoArgumentException;
+
 public class IndexController {
 
     public String index(){
@@ -7,7 +9,10 @@ public class IndexController {
         return "index";
     }
 
-    public String oupsHandler(){
-        return "notimplemented";
+    public String oupsHandler(String name){
+        if (name != "method") {
+            throw new OupsHandlerWithNoArgumentException("No string name passed!");
+        }
+        return "notimplemented " + name;
     }
 }
--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.controllers.exceptions.OupsHandlerWithNoArgumentException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,7 +22,13 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("Annotation to put another meaningful name for the tests")
     void oupsHandler() {
-        assertEquals("notimplemented", indexController.oupsHandler(), () -> "Some Havier Work To Do!");
+        assertEquals("notimplemented method", indexController.oupsHandler("method"), () -> "Some Havier Work To Do!");
+    }
+
+    @Test
+    void oupsHandlerException() {
+        assertThrows(OupsHandlerWithNoArgumentException.class, () -> indexController.oupsHandler("variable"));
     }
 }
