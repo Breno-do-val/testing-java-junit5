0f473724076f22158d5b8b756119cce4b5e309c1
Breno Ribeiro do Val <brenorvale@gmail.com>
2021-08-31 14:41:50 -0300

    (test): final assignment

--- src/test/java/guru/springframework/sfgpetclinic/fauxspring/ModelMapImpl.java
+++ src/test/java/guru/springframework/sfgpetclinic/fauxspring/ModelMapImpl.java
@@ -1,2 +1,23 @@
-package guru.springframework.sfgpetclinic.fauxspring;public class ModelMapImpl {
+package guru.springframework.sfgpetclinic.fauxspring;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelMapImpl implements Model{
+
+    Map<String, Object> map = new HashMap<>();
+    @Override
+    public void addAttribute(String key, Object o) {
+        map.put(key, o);
+    }
+
+    @Override
+    public void addAttribute(Object o) {
+        // do nothing
+    }
+
+
+    public Map<String, Object> getMap() {
+        return this.map;
+    }
 }
